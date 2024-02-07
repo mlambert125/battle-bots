68f298c5b7acfa151010b4249fdee86992b362f4
Michael Lambert <mlambert125@live.com>
2024-02-07 16:50:05 -0500

    Improved blue bot

--- src/main/java/org/example/BlueBot.java
+++ src/main/java/org/example/BlueBot.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class BlueBot extends BaseBot {
+    public int defend_counter = 0;
+
     public BlueBot() {
         super("BlueBot");
     }
@@ -10,7 +12,13 @@ public class BlueBot extends BaseBot {
         // or reloads them if it has none.
 
         if (enemy.getRocketCount() > 0) {
-            return BaseBot.DEFEND;
+            if (defend_counter < 3) {
+                defend_counter++;
+                return BaseBot.DEFEND;
+            } else {
+                defend_counter = 0;
+                return BaseBot.ATTACK;
+            }
         } else if (this.getRocketCount() > 0) {
             return BaseBot.FIRE_ROCKET;
         } else {
